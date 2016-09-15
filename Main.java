c128846b961eabbbec03718ed122ddad7b561b5e
Amon-shi <tihonov_97_1997@mail.ru>
2016-09-15 22:12:58 +0300

    Insertion Sort

---
+++ Main.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Игорь on 12.09.2016.
+ */
+import java.util.Random;
+public class Main{
+    public static void insertionSort(int[] arr) {
+        for(int i = 1; i < arr.length; i++){
+            int currElem = arr[i];
+            int prevKey = i - 1;
+            while(prevKey >= 0 && arr[prevKey] > currElem){
+                arr[prevKey+1] = arr[prevKey];
+                prevKey--;
+            }
+            arr[prevKey+1] = currElem;
+        }
+    }
+    public static void main(){
+
+        int[] c = new int[100];
+        for(int k=0; k < 100; k++ ){
+            c[k]=1;
+        }
+
+        insertionSort(c);
+
+    }
+}
